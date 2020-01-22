fffa36d2d881acbf64eb4763cc2a0bf4bf4fca22
Nazrin Shah Adrian Fernandez <nazrinfernandez@gmail.com>
2020-01-22 11:08:12 +0800

    added error pages

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -11,10 +11,13 @@ assets.register(bundles)
 app.register_blueprint(users_blueprint, url_prefix="/users")
 app.register_blueprint(sessions_blueprint)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 @app.route("/")
 def home():
